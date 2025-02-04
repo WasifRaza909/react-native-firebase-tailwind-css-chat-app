20d880cb240b8f7376eb11196e6c8b32abb89a6b
wasif-technyx <142510796+wasif-technyx@users.noreply.github.com>
2025-02-04 09:06:09 +0500

    Init commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View className='bg-red-200 pt-20'>
+      <Text className='text-3xl text-center'>Home</Text>
+    </View>
+  )
+}
+
+export default index
